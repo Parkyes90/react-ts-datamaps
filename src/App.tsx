f8e6281f45148e1e59892427f905eaa17572e533
ys.park <ys.park@datadriven.kr>
2021-01-13 10:39:16 +0900

    DataMap Filter overlay

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,44 @@
 import React, { useEffect, useRef } from "react";
 import DataMap from "datamaps";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Map = styled.div`
+  height: 100%;
+  width: 100%;
+`;
+
 function App() {
   const mapRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
     if (mapRef.current) {
       const map = new DataMap({
         element: mapRef.current,
+        projection: "mercator",
         fills: {
-          defaultFill: "rgba(23,48,210, 0.9)",
+          defaultFill: "#ABDDA4",
+          authorHasTraveledTo: "#fa0fa0",
         },
+      });
+      map.updateChoropleth({
+        USA: "red",
       });
     }
     return () => {
       console.log("de");
     };
-  }, []);
+  }, [mapRef]);
   return (
-    <div
-      style={{
-        height: "100vh",
-      }}
-      ref={mapRef}
-    >
-      test
-    </div>
+    <Container>
+      <Map ref={mapRef} />
+    </Container>
   );
 }
 
